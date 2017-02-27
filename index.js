1000ba01695ad77074b62184386457859a01bc97
Levi Goltz <roguezedd@Levi-Goltzs-MacBook-Pro.local>
2017-02-26 17:05:14 -0800

    Switch to marko.

--- index.js
+++ index.js
@@ -1,24 +1,11 @@
 var Metalsmith  = require('metalsmith');
 var assets      = require('metalsmith-assets');
 var collections = require('metalsmith-collections');
-var handlebars  = require('handlebars');
+var marko       = require('marko');
 var images      = require("metalsmith-scan-images");
 var layouts     = require('metalsmith-layouts');
 var markdown    = require('metalsmith-markdown');
 var permalinks  = require('metalsmith-permalinks');
-
-handlebars.registerHelper("debug", function(optionalValue) {
-  if (optionalValue) {
-    console.log("Value");
-    console.log("====================");
-    console.log(optionalValue);
-  }
-});
-
-handlebars.registerHelper("mdToHtml", function(val) {
-  val = val || '';
-  return val.replace('.md', '.html');
-});
 
 Metalsmith(__dirname)
   .clean(false)
@@ -37,7 +24,7 @@ Metalsmith(__dirname)
   }))
   .use(markdown())
   .use(layouts({
-    engine: 'handlebars',
+    engine: 'marko',
     partials: 'partials',
     directory: 'templates'
   }))
