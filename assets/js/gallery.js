cf8ebe280105d274b6d202b802df73e6169de6da
Levi Goltz <roguezedd@Levi-Goltzs-MacBook-Pro.local>
2017-03-02 22:21:54 -0800

    Gallery refinement.

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,47 +1,68 @@
+var lightboxClasses = {
+	CONTAINER: 'fullimg-container',
+	CONTAINER_HIDDEN: 'fullimg-container-hidden',
+	FULL_IMG: 'fullimg',
+	CLOSE_BTN: 'closebtn',
+	CLOSE_BTN_HIDDEN: 'closebtn-hidden'
+};
+
+function createLightbox(){
+	fullImgContainer = document.createElement('div');
+	fullImgContainer.className = lightboxClasses.CONTAINER;
+
+	fullImg = document.createElement('img');
+	fullImg.className = lightboxClasses.FULL_IMG;
+	fullImg.onload = onFullImgLoad;
+
+	closeBtn = document.createElement('span');
+	closeBtn.className = lightboxClasses.CLOSE_BTN  + ' ' + lightboxClasses.CLOSE_BTN_HIDDEN;
+	closeBtn.innerHTML = 'x';
+	closeBtn.onclick = onCloseFullImgClick;
+
+	fullImgContainer.appendChild(fullImg);
+	fullImgContainer.appendChild(closeBtn);
+	document.body.appendChild(fullImgContainer);
+}
+
+function findLightboxElements(){
+	return {
+		fullImgContainer: document.querySelector('.' + lightboxClasses.CONTAINER),
+		fullImg: document.querySelector('.'+ lightboxClasses.CONTAINER + ' .' + lightboxClasses.FULL_IMG),
+		closeBtn: document.querySelector('.'+ lightboxClasses.CONTAINER + ' .' + lightboxClasses.CLOSE_BTN)
+	};
+}
+
 function onThumbnailClick(e){
 	var imgSrc = e.target.attributes['data-url'].value;
-	var fullImgContainer = document.querySelector("body .fullimg-container");
-	var fullImg, closeBtn;
+	var fullImgContainer = document.querySelector('.' + lightboxClasses.CONTAINER);
+	var lightboxElements;
 
 	if(!fullImgContainer){
-		fullImgContainer = document.createElement('div');
-		fullImgContainer.className = 'fullimg-container';
-
-		fullImg = document.createElement('img');
-		fullImg.className = 'fullimg';
-		fullImg.onload = onFullImgLoad;
-
-		closeBtn = document.createElement('span');
-		closeBtn.className = 'closebtn closebtn-hidden';
-		closeBtn.innerHTML = '[x]';
-		closeBtn.onclick = onCloseFullImgClick;
-
-		fullImgContainer.appendChild(fullImg);
-		fullImgContainer.appendChild(closeBtn);
-		document.body.appendChild(fullImgContainer);
-	} else {
-		fullImg = document.querySelector("body .fullimg-container .fullimg");
-		closeBtn = document.querySelector("body .fullimg-container .closebtn");
+		createLightbox();
 	}
 
-	fullImg.src = imgSrc;
-	closeBtn.className = 'closebtn closebtn-hidden';
-	fullImgContainer.className = 'fullimg-container';
+	lightboxElements = findLightboxElements();
+
+	lightboxElements.fullImg.src = imgSrc;
+	lightboxElements.closeBtn.className = lightboxClasses.CLOSE_BTN  + ' ' + lightboxClasses.CLOSE_BTN_HIDDEN;
+	lightboxElements.fullImgContainer.className = lightboxClasses.CONTAINER;
 }
 
 function onFullImgLoad(){
-	var closeBtn = document.querySelector("body .fullimg-container .closebtn");
-	var fullImg = document.querySelector("body .fullimg-container .fullimg");
+	var lightboxElements = findLightboxElements();
+	var translateX, translateY;
 
 	if(closeBtn && fullImg){
 		setTimeout(function() {
-			closeBtn.style.transform = 'translate(' + Math.floor(fullImg.width/2 - 17) + 'px, -' + Math.floor(fullImg.height/2 + 25) + 'px)';
-			closeBtn.className = 'closebtn';
+			translateX = Math.floor(lightboxElements.fullImg.width/2 - 4);
+			translateY = Math.floor(lightboxElements.fullImg.height/2 + 16);
+			lightboxElements.closeBtn.style.transform = 'translate(' + translateX + 'px, -' + translateY + 'px)';
+			lightboxElements.closeBtn.className = lightboxClasses.CLOSE_BTN;
 		}, 100);
 	}
 }
 
 function onCloseFullImgClick(){
-	var fullImgContainer = document.querySelector("body .fullimg-container");
-	fullImgContainer.className = 'fullimg-container fullimg-container-hidden';
+	lightboxElements = findLightboxElements();
+	lightboxElements.fullImgContainer.className = lightboxClasses.CONTAINER + ' ' + lightboxClasses.CONTAINER_HIDDEN;
 }
